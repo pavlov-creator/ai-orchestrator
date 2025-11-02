52eb1c7d04fd86bb3a90661d86e83adee91d427b
pavlov-creator <pavlovniay@gmail.com>
2025-11-02 13:38:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "ai-orchestrator is running"
+    return "ai-orchestrator is alive"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
