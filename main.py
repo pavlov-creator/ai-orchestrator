da665ef32436db40a910486346bd5781e8924e27
pavlov-creator <pavlovniay@gmail.com>
2025-11-02 13:12:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ok"}
+    return {"ok": True}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
 
 
